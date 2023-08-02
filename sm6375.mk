776f08521b34c3fbf80a2e68c03e8ed0b9bc4bbd
Vivekachooz <vivekachooz@gmail.com>
2023-08-03 00:15:21 +0530

    sm6375-common: Build fingerprint service from hardware/oplus
    Change-Id: Icae68608f64f7115ccd9dac3d6c3d7f1d0617f36

--- sm6375.mk
+++ sm6375.mk
@@ -145,6 +145,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     fastbootd
 
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.3-service.oplus
+
 # Gatekeeper
 PRODUCT_PACKAGES += \
     android.hardware.gatekeeper@1.0.vendor
