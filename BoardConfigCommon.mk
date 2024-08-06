9f40d85d50e7890000031f480aff6703161dfe3c
Vivekachooz <vivekachooz@gmail.com>
2024-08-06 23:30:27 +0530

    fixup! sm6375-common: Define boot image security patch
    Change-Id: I73d65d53465eea9eb18daac04af2ac4ff5889036

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -180,7 +180,7 @@ include hardware/oplus/sepolicy/qti/SEPolicy.mk
 BOARD_VENDOR_SEPOLICY_DIRS += $(COMMON_PATH)/sepolicy/vendor
 
 # Vendor Security patch level
-VENDOR_SECURITY_PATCH := 2024-05-05
+BOOT_SECURITY_PATCH := 2024-05-05
 VENDOR_SECURITY_PATCH := $(BOOT_SECURITY_PATCH)
 
 # Verified Boot
