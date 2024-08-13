c1b01e885f62db286d3397e2bc2110d203b2a13c
Albert Tang <tangalbert919@yahoo.com>
2024-08-13 11:01:41 -0500

    sm6375-common: Add support for gunnar
    Change-Id: I5a36833078843599c9a9058ed5b0ee5ca24c330e

--- Android.mk
+++ Android.mk
@@ -6,7 +6,7 @@
 
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter larry,$(TARGET_DEVICE)),)
+ifneq ($(filter larry gunnar,$(TARGET_DEVICE)),)
 include $(call all-makefiles-under,$(LOCAL_PATH))
 
 include $(CLEAR_VARS)
