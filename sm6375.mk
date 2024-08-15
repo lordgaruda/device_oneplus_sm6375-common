f65dbbff07e10718b487f9933b9806d6be469dc5
Albert Tang <tangalbert919@yahoo.com>
2024-08-15 10:40:05 -0500

    sm6375-common: Decommonize hals.conf
    Change-Id: I232827f2a2523e6fb5672fa5b73d9e33ae64fc0b

--- sm6375.mk
+++ sm6375.mk
@@ -388,9 +388,6 @@ PRODUCT_PACKAGES += \
     android.hardware.sensors@2.0-service.multihal \
     libsensorndkbridge
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/sensors/hals.conf:$(TARGET_COPY_OUT_VENDOR)/etc/sensors/hals.conf
-
 # Shipping API
 BOARD_SHIPPING_API_LEVEL := 30
 PRODUCT_SHIPPING_API_LEVEL := 31
