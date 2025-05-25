c8d39657d8dee247050cde3202653e409afd9100
Vivekachooz <vivekachooz@gmail.com>
2025-05-25 22:57:24 +0530

    sm6375-common: Change device model for all ro.product sources
    Change-Id: I3cb6cd56cf3a59dfb05cca2153118e34f2d7dada

--- init/init_oplus.cpp
+++ init/init_oplus.cpp
@@ -13,19 +13,22 @@ using android::base::GetProperty;
 
 /*
  * SetProperty does not allow updating read only properties and as a result
- * does not work for our use case. Write "OverrideProperty" to do practically
+ * does not work for our use case. Write "property_override" to do practically
  * the same thing as "SetProperty" without this restriction.
  */
-void OverrideProperty(const char* name, const char* value) {
-    size_t valuelen = strlen(value);
-
-    prop_info* pi = (prop_info*)__system_property_find(name);
+void property_override(const std::string& prop, const std::string& value, bool add = true) {
+    prop_info* pi = (prop_info*)__system_property_find(prop.c_str());
     if (pi != nullptr) {
-        __system_property_update(pi, value, valuelen);
-    } else {
-        __system_property_add(name, strlen(name), value, valuelen);
+        __system_property_update(pi, value.c_str(), value.length());
+    } else if (add) {
+        __system_property_add(prop.c_str(), prop.length(), value.c_str(), value.length());
     }
 }
+
+const std::vector<std::string> ro_props_default_source_order = {
+    "bootimage.", "odm.", "product.", "system.",
+    "system_ext.", "vendor.", "vendor_dlkm.", "",
+};
 
 /*
  * Only for read-only properties. Properties that can be wrote to more
@@ -37,35 +40,37 @@ void vendor_load_properties() {
     auto prjname = std::stoi(GetProperty("ro.boot.prjname", "0"));
     auto sku = std::stoi(GetProperty("ro.boot.product.hardware.sku", "0"));
 
-    switch (prjname) {
-        // gunnar
-        case 20826: // T-Mobile (20826)
-            OverrideProperty("ro.product.product.model", "GN2200");
-            break;
-        default:
-            LOG(ERROR) << "Unexpected project name: " << prjname;
-    }
+    for (const auto& source : ro_props_default_source_order) {
+        switch (prjname) {
+            // gunnar
+            case 20826: // T-Mobile (20826)
+                property_override("ro.product." + source + "model", "GN2200");
+                break;
+            default:
+                LOG(ERROR) << "Unexpected project name: " << prjname;
+        }
 
-    switch (sku) {
-        // larry/oscaro
-        case 2: // IN
-            if (device == "OP535DL1") {
-                OverrideProperty("ro.product.product.model", "CPH2381");
-            } else if (device == "OP5958L1") {
-                OverrideProperty("ro.product.product.model", "CPH2467");
-            }
-            break;
-        case 6: // GL
-            if (device == "OP535DL1") {
-                OverrideProperty("ro.product.product.model", "CPH2409");
-            } else if (device == "OP5958L1") {
-                OverrideProperty("ro.product.product.model", "CPH2465");
-            }
-            break;
-        case 14: // NA
-            OverrideProperty("ro.product.product.model", "CPH2513");
-            break;
-        default:
-            LOG(ERROR) << "Unexpected SKU: " << sku;
+        switch (sku) {
+            // larry/oscaro
+            case 2: // IN
+                if (device == "OP535DL1") {
+                    property_override("ro.product." + source + "model", "CPH2381");
+                } else if (device == "OP5958L1") {
+                    property_override("ro.product." + source + "model", "CPH2467");
+                }
+                break;
+            case 6: // GL
+                if (device == "OP535DL1") {
+                    property_override("ro.product." + source + "model", "CPH2409");
+                } else if (device == "OP5958L1") {
+                    property_override("ro.product." + source + "model", "CPH2465");
+                }
+                break;
+            case 14: // NA
+                property_override("ro.product." + source + "model", "CPH2513");
+                break;
+            default:
+                LOG(ERROR) << "Unexpected SKU: " << sku;
+        }
     }
 }
